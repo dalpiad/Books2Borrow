db895809b29b231368c38ecad4cf7d48a9be1257
Arpitha Nagappa <arpithagn021@gmail.com>
2024-03-17 00:33:28 -0500

    Spring Initialize

---
+++ src/main/java/org/launchcode/books2borrow/Books2borrowApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.books2borrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Books2borrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Books2borrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/books2borrow/Books2borrowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.books2borrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Books2borrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
