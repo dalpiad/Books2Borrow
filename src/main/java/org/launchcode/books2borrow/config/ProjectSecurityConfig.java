8dcd3586e24cd920c146207fe94af12bf27b3dfe
Jessica Orrick <jessica.orrick@outlook.com>
2024-03-26 20:53:55 -0500

    added config package, ProjectSecurityConfig class for authenticating routes

---
+++ src/main/java/org/launchcode/books2borrow/config/ProjectSecurityConfig.java
@@ -0,0 +1,24 @@
+package org.launchcode.books2borrow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+    @Configuration
+    public class ProjectSecurityConfig {
+
+        @Bean
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+            //custom security configuration for authenticating and whitelisting routes
+            http.authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/").authenticated()
+                            .requestMatchers("/hello").permitAll())
+                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults());
+            return http.build();
+        }
+    }
+
