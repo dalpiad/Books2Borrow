b50c8465e7a146e5f39887e72e8a34b2ffcac850
Jessica Orrick <jessica.orrick@outlook.com>
2024-03-30 22:45:14 -0500

    Combining two branches to catch up to backend-dev

--- src/main/java/org/launchcode/books2borrow/controllers/UserController.java
+++ src/main/java/org/launchcode/books2borrow/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/hello")
     public String sayHello(){
-        return "This should be visible without logging in with Spring Security";
+        return "This should be visible without logging in with Spring Security" ;
     }
 
     @GetMapping("/testing")
