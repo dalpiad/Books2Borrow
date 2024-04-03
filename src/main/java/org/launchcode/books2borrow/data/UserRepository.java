ae3bc4392f0e2b8a7e79e6a098166850cb1261f4
dalpiad <dalpiad@gmail.com>
2024-04-03 11:48:53 -0500

    Fixed errors in UserRepo (my edits), and MessageRepo.

--- src/main/java/org/launchcode/books2borrow/data/UserRepository.java
+++ src/main/java/org/launchcode/books2borrow/data/UserRepository.java
@@ -1,14 +1,13 @@
 package org.launchcode.books2borrow.data;
 
 import org.launchcode.books2borrow.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
 }
