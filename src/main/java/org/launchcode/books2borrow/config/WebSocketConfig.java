ed432e7b816d81e6aecedcb3010b1550afead258
dalpiad <dalpiad@gmail.com>
2024-04-09 18:14:01 -0500

    rebuilt gradle to import WebSocketConfig

--- src/main/java/org/launchcode/books2borrow/config/WebSocketConfig.java
+++ src/main/java/org/launchcode/books2borrow/config/WebSocketConfig.java
@@ -1,8 +1,8 @@
 package org.launchcode.books2borrow.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
