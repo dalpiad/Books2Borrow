6a4e6c14146631b70d41bc686a79c7181fb5a045
Jessica Orrick <jessica.orrick@outlook.com>
2024-04-16 20:27:37 -0500

    basic functioning login, register, testing.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function LoginForm() {
+    let navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -31,6 +34,7 @@ function LoginForm() {
             console.error('Response status:', error.response ? error.response.status : 'No response');
             console.error('Response data:', error.response ? error.response.data : 'No response data');
         }
+        navigate("/")
     };
 
     return (
@@ -49,62 +53,3 @@ function LoginForm() {
 }
 
 export default LoginForm;
-
-
-
-
-
-
-// import { useState } from 'react';
-// import axios from 'axios';
-
-// function LoginForm() {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-//         const authToken = btoa(`${email}:${password}`); // Base64 encode email and password
-//         try {
-//             const response = await axios.get('http://localhost:8080/user', {
-//                 headers: {
-//                     'Authorization': `Basic ${authToken}`
-//                 }
-//             });
-//             console.log('Response:', response.data);
-
-//             // Save Authorization header to local storage if present
-//             const authHeader = response.headers['authorization'];
-//             if (authHeader) {
-//                 localStorage.setItem('Authorization', authHeader);
-//                 console.log('Authorization header saved to local storage.');
-//             }
-//         } catch (error) {
-//             console.error('Error fetching data:', error);
-//         }
-//     };
-
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <div>
-//                 <label>Email:</label>
-//                 <input
-//                     type="email"
-//                     value={email}
-//                     onChange={e => setEmail(e.target.value)}
-//                 />
-//             </div>
-//             <div>
-//                 <label>Password:</label>
-//                 <input
-//                     type="password"
-//                     value={password}
-//                     onChange={e => setPassword(e.target.value)}
-//                 />
-//             </div>
-//             <button type="submit">Login</button>
-//         </form>
-//     );
-// }
-
-// export default LoginForm;
