5e157ab2ed713bf16bbd54823dd537e9edd0ccb1
Matt Speaks <mattspeaks@hotmail.com>
2024-04-25 07:32:24 -0500

    Cleanup checkout card to remove duplicate information.

--- src/components/ui/CheckoutCard.jsx
+++ src/components/ui/CheckoutCard.jsx
@@ -7,11 +7,11 @@ const CheckoutCard = ({ selectedRecord }) => {
 
     return (
         <div>
-            <div>{`Book Title: ${checkoutRecord.bookTitle}`}</div>
-            <div>Lender Name{`Lender Name: ${checkoutRecord.lenderName}`}</div>
-            <div>Borrower Name{`Borrower Name: ${checkoutRecord.borrowerName}`}</div>
-            <div>Checkout Date{`Checkout Date: ${checkoutRecord.checkoutDate.slice(0,10)}`}</div>
-            <div>Due Date{`Due Date: ${checkoutRecord.dueDate.slice(0,10)}`}</div>
+            <div>{`Title: ${checkoutRecord.bookTitle}`}</div>
+            <div>{`Lender : ${checkoutRecord.lenderName}`}</div>
+            <div>{`Borrower: ${checkoutRecord.borrowerName}`}</div>
+            <div>{`Checkout Date: ${checkoutRecord.checkoutDate.slice(0,10)}`}</div>
+            <div>{`Due Date: ${checkoutRecord.dueDate.slice(0,10)}`}</div>
         </div>
     )
 }
